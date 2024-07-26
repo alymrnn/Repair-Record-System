ce0eb5b4119322476de032bbc36548c0aab114ec
alymrnn <allym.furukawa@gmail.com>
2024-07-26 14:38:44 +0800

    fixed update acct ui

--- modals/update_account.php
+++ modals/update_account.php
@@ -1,7 +1,7 @@
 <div class="modal fade bd-example-modal-xl" id="update_account" tabindex="-1" role="dialog"
     aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content" style="background:#e9e9e9;">
+        <div class="modal-content" style="background:#f9f9f9;">
             <div class="modal-header" style="background:#343a40;">
                 <h5 class="modal-title" id="exampleModalLabel" style="font-weight: normal;color: #fff;">
                     Update Account Details
@@ -12,7 +12,7 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-sm-2">
+                    <div class="col-sm-4">
                         <!-- id -->
                         <input type="hidden" id="id_account_update" class="form-control">
                         <!-- employee id -->
@@ -26,21 +26,37 @@
                         <input type="text" id="full_name_update" class="form-control pl-3" autocomplete="off"
                             style="color: #525252;font-size: 15px;border-radius: .25rem;border: 1px solid #525252;background: #FFF;height:34px; width:100%;">
                     </div>
-                    <div class="col-sm-3">
+                    <div class="col-sm-4">
                         <!-- department -->
                         <label style="font-weight: normal;color: #000;">Department</label>
-                        <input type="text" id="department_update" class="form-control pl-3" autocomplete="off"
+                        <select id="department_update" class="form_control pl-2"
                             style="color: #525252;font-size: 15px;border-radius: .25rem;border: 1px solid #525252;background: #FFF;height:34px; width:100%;">
+                            <option value="" selected disabled>Select Department</option>
+                            <option value="IT">IT</option>
+                            <option value="PD1">PD1</option>
+                            <option value="PD2">PD2</option>
+                            <option value="QA">QA</option>
+                            <option value="QC">QC</option>
+                        </select>
                     </div>
-                    <div class="col-sm-3">
-                        <!-- section -->
-                        <label style="font-weight: normal;color: #000;">Section</label>
-                        <input type="text" id="section_update" class="form-control pl-3" autocomplete="off"
-                            style="color: #525252;font-size: 15px;border-radius: .25rem;border: 1px solid #525252;background: #FFF;height:34px; width:100%;">
-                    </div>
+
                 </div>
                 <br>
                 <div class="row">
+                    <div class="col-sm-4">
+                        <!-- section -->
+                        <label style="font-weight: normal;color: #000;">Section</label>
+                        <select id="section_update" class="form_control pl-2"
+                            style="color: #525252;font-size: 15px;border-radius: .25rem;border: 1px solid #525252;background: #FFF;height:34px; width:100%;">
+                            <option value="" selected disabled>Select Section</option>
+                            <option value="FAP1">FAP1</option>
+                            <option value="FAP2">FAP2</option>
+                            <option value="FAP3">FAP3</option>
+                            <option value="FAP4">FAP4</option>
+                            <option value="QA">QA</option>
+                            <option value="QC">QC</option>
+                        </select>
+                    </div>
                     <div class="col-sm-4">
                         <!-- password -->
                         <label style="font-weight: normal;color: #000;">Password</label>
@@ -57,6 +73,7 @@
                             <option value="IT">IT</option>
                             <option value="PD">PD</option>
                             <option value="QC">QC</option>
+                            <option value="PD Verifier">PD Verifier</option>
                         </select>
                     </div>
                     <div class="col-3">
